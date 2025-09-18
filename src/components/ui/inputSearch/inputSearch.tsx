8ee2b3c837f7834f030ccb780d8e46e72cfa5ec0
JasonAraujo1 <jasonaraujo123@hotmail.com>
2025-09-18 14:22:16 -0300

    adiciona componente de busca e melhora a exibição dos títulos na página inicial

---
+++ src/components/ui/inputSearch/inputSearch.tsx
@@ -0,0 +1,9 @@
+
+export default function InputSearch() {
+  return (
+    <div>
+        
+        <input type="text" />
+    </div>
+  )
+}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,27 +1,34 @@
 import { useEffect, useState } from "react"
 import { fetchApiTitle } from "../../services/fetchApi.js"
+import "./Home.css"
+import InputSearch from "../../components/ui/inputSearch/inputSearch.js"
 
 export default function Home() {
 
-  const [ titles , setTitles] = useState([])
+  const [titles, setTitles] = useState([])
 
-  useEffect(()=>{
-    async function titleData(){
+  useEffect(() => {
+    async function titleData() {
       const data = await fetchApiTitle()
-      setTitles(data.docs) 
+      setTitles(data.docs)
     }
     titleData()
-  },[])
+  }, [])
   console.log(titles)
 
   return (
-    <div>{titles.map((item)=>(
-      <div>
-        <h1>{item.author_name}</h1>
-        <h3>{item.title}</h3>
-        <br />
+    <div >
+      <InputSearch />
+      <div className="divcontainer">{titles.map((item) => (
+        <div >
+          <div className="divBody">
+            <h1>{item.author_name}</h1>
+            <h3>{item.title}</h3>
+            <br />
+          </div>
+        </div>
+      ))}
       </div>
-    ))}
-    </div> 
+    </div>
   )
 }
