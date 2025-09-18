dfe7006c16894b8395e0bc81010c1c6320f026d6
JasonAraujo1 <jasonaraujo123@hotmail.com>
2025-09-18 14:45:46 -0300

    adiciona funcionalidade de busca ao componente de entrada

--- src/components/ui/inputSearch/inputSearch.tsx
+++ src/components/ui/inputSearch/inputSearch.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react"
 import "./inputSearch.css"
 export default function InputSearch() {
-  return (
-    <div className="divInput">
-        <span>Busque por um livro</span>
-        <input type="text" />
-    </div>
-  )
+
+    const [inputData, setInputData] = useState("")
+
+    console.log(inputData)
+
+    function handleBuscar(){
+        
+    }
+
+    return (
+        <div className="divInput">
+            <span>Busque por um livro</span>
+            <input type="text" onChange={(event) => setInputData(event.target.value)} />
+            <button onClick={handleBuscar}>Buscar</button>
+        </div>
+    )
 }
