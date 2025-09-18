f94e8231d908c9464b1454d507dfbe02cda404fe
JasonAraujo1 <jasonaraujo123@hotmail.com>
2025-09-18 14:24:20 -0300

    adiciona estilo ao componente de busca e ajusta margens na página inicial

--- src/components/ui/inputSearch/inputSearch.tsx
+++ src/components/ui/inputSearch/inputSearch.tsx
@@ -1,8 +1,8 @@
-
+import "./inputSearch.css"
 export default function InputSearch() {
   return (
-    <div>
-        
+    <div className="divInput">
+        <span>Busque por um livro</span>
         <input type="text" />
     </div>
   )
